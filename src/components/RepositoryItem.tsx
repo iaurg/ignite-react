144cac7ec64f1e3c77a58d77883ffedc1bdbda0e
Italo Aurelio <italoaurelior@gmail.com>
2021-03-15 20:58:11 -0300

    Finish chapter one

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type RepositoryItemProps = {
+  repository:{
+    name: string
+    description: string
+    html_url: string
+  }  
+}
+
+export default function RepositoryItem(props: RepositoryItemProps) {
+  return (
+    <li>
+      <strong>{props.repository.name ?? 'Default'}</strong>
+      <p>{props.repository.description}</p>
+      <a href={props.repository.html_url}>Link</a>
+    </li>
+  )
+}
