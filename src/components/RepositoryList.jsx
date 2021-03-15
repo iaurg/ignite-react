fab34b430521284335fb49ef8bcd9883e5c14aed
Italo Aurelio <italoaurelior@gmail.com>
2021-03-14 21:16:11 -0300

    Primeiro component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Form in react</p>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Form in react</p>
+        </li>
+      </ul>
+    </section>
+  )
+}
